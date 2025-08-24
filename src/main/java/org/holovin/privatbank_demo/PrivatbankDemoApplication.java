d39522461ce8c5b68dd3fa7cf664901da30c362c
Bohdan Holovin <b.holovin@gmail.com>
2025-08-24 19:30:13 +0300

    Init

---
+++ src/main/java/org/holovin/privatbank_demo/PrivatbankDemoApplication.java
@@ -0,0 +1,13 @@
+package org.holovin.privatbank_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatbankDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivatbankDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/holovin/privatbank_demo/PrivatbankDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.holovin.privatbank_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivatbankDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
