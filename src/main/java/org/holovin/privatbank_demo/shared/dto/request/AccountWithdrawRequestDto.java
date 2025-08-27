bf09481dc202a5cc09d12ae83ec54f30a05b0667
Bohdan Holovin <58120983+bohdan-holovin@users.noreply.github.com>
2025-08-27 09:58:05 +0300

    PB-7 Add withdraw (#6)
    * PB-7 Add withdraw
    
    * Refactor

---
+++ src/main/java/org/holovin/privatbank_demo/shared/dto/request/AccountWithdrawRequestDto.java
@@ -0,0 +1,23 @@
+package org.holovin.privatbank_demo.shared.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountWithdrawRequestDto {
+
+    @NotBlank
+    private String accountNumber;
+
+    @NotNull
+    @Positive
+    private BigDecimal amount;
+}
